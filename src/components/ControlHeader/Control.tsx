efdcf12951639a6fc8643a26d9f5e54a21d30ff9
jvdalaglio <jvdalaglio@gmail.com>
2023-09-07 23:11:19 -0300

    configurando back local

--- src/components/ControlHeader/Control.tsx
+++ src/components/ControlHeader/Control.tsx
@@ -11,49 +11,54 @@ import { StoreState } from "../../redux";
 import { CustomLink } from "../Card/Card.style";
 
 export type ControlHeaderProps = {
-  name?: string;
-  text: string;
-  show?: boolean;
-  price: number;
-  tableId:number | string | null;
-  idProduct: number;
-}
-
+ name?: string;
+ text: string;
+ show?: boolean;
+ price: number;
+ tableId: number | string | null;
+ id: number;
+};
 
 export function ControlHeader(props: ControlHeaderProps) {
-  const { id } = useParams();
-  const  dispatch = useDispatch();
-  const [tables, setTables] = useState<TableCardProps[]>([]);
+ const { id } = useParams();
+ const dispatch = useDispatch();
+ const [tables, setTables] = useState<TableCardProps[]>([]);
 
-  async function getTables() {
-    const {data} = await api.get("tables");
-    setTables(data)
-  }
-  useEffect(() => {
-    getTables();
-  });
+ async function getTables() {
+  const { data } = await api.get("tables");
+  setTables(data);
+ }
+ useEffect(() => {
+  getTables();
+ });
 
-  function handleClick() {
-  }
+ function handleClick() {}
 
-  return (
-    <>
-    <Header hasGoBack />
-    <ControlStyle>
-      <div className="title">
-        <strong>{props.name}</strong>
-        <TableCard id={props.tableId}/>
-      </div>
-      <CustomLink to={"/confirmed"}>
-      <TotalCard onClick={handleClick} text={props.text} price={props.price} tableId={props.tableId} idProduct={props.idProduct}/>
-      </CustomLink>
-      {props.show && 
-      <div className="checkout">
-        <button className="checkout-text">Confira seu pedido abaixo antes de enviar</button>
-      </div>
-      }
-    </ControlStyle>
-    </>
-
-  )
+ return (
+  <>
+   <Header hasGoBack />
+   <ControlStyle>
+    <div className="title">
+     <strong>{props.name}</strong>
+     <TableCard id={props.tableId} />
+    </div>
+    <CustomLink to={"/confirmed"}>
+     <TotalCard
+      onClick={handleClick}
+      text={props.text}
+      price={props.price}
+      tableId={props.tableId}
+      id={props.id}
+     />
+    </CustomLink>
+    {props.show && (
+     <div className="checkout">
+      <button className="checkout-text">
+       Confira seu pedido abaixo antes de enviar
+      </button>
+     </div>
+    )}
+   </ControlStyle>
+  </>
+ );
 }
--- src/pages/Bill.tsx
+++ src/pages/Bill.tsx
@@ -2,14 +2,20 @@ import { ControlHeader } from "../components/ControlHeader/Control";
 import { MenuBottom } from "../components/MenuBottom/MenuBottom";
 
 export function Bill() {
-  const urlParams = new URLSearchParams(location.search);
+ const urlParams = new URLSearchParams(location.search);
 
-  const id = urlParams.get('table');
+ const id = urlParams.get("table");
 
-  return(
-    <>
-    <ControlHeader text="Fechar conta" name="Conta" idProduct={1} tableId={id} price={0} />
-    <MenuBottom />
-    </>
-  )
+ return (
+  <>
+   <ControlHeader
+    text="Fechar conta"
+    name="Conta"
+    id={1}
+    tableId={id}
+    price={0}
+   />
+   <MenuBottom />
+  </>
+ );
 }
